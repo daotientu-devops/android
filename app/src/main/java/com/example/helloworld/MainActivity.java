a82a2ffd9fb1f456f171fcc20402b4c1a69ec871
tudao <tudao@5nhatnhat.com>
2021-08-10 00:06:12 +0700

    - [Android]: FrameLayout là loại layout đơn giản mà các View con chứa bên trong xếp chồng lên nhau và định vị bằng thuộc tính layout_gravity android:gravity để căn chỉnh các View nằm ở vị trí nào trong Layout

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -51,48 +51,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        imageView = findViewById(R.id.image);
-        textView = findViewById(R.id.textview);
-        findViewById(R.id.btn_center).setOnClickListener(clickListener);
-        findViewById(R.id.btn_centerCrop).setOnClickListener(clickListener);
-        findViewById(R.id.btn_centerInside).setOnClickListener(clickListener);
-        findViewById(R.id.btn_fitCenter).setOnClickListener(clickListener);
-        findViewById(R.id.btn_fitEnd).setOnClickListener(clickListener);
-        findViewById(R.id.btn_fitStart).setOnClickListener(clickListener);
-        findViewById(R.id.btn_fitXY).setOnClickListener(clickListener);
     }
-
-    View.OnClickListener clickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            ImageView.ScaleType scaleType = ImageView.ScaleType.CENTER;
-            switch (v.getId()) {
-                case R.id.btn_center:
-                    scaleType = ImageView.ScaleType.CENTER;
-                    break;
-                case R.id.btn_centerCrop:
-                    scaleType = ImageView.ScaleType.CENTER_CROP;
-                    break;
-                case R.id.btn_centerInside:
-                    scaleType = ImageView.ScaleType.CENTER_INSIDE;
-                    break;
-                case R.id.btn_fitCenter:
-                    scaleType = ImageView.ScaleType.FIT_CENTER;
-                    break;
-                case R.id.btn_fitEnd:
-                    scaleType = ImageView.ScaleType.FIT_END;
-                    break;
-                case R.id.btn_fitStart:
-                    scaleType = ImageView.ScaleType.FIT_START;
-                    break;
-                case R.id.btn_fitXY:
-                    scaleType = ImageView.ScaleType.FIT_XY;
-                    break;
-            }
-            imageView.setScaleType(scaleType);
-            textView.setText(((Button)v).getText());
-        }
-    };
 
     public void run(View view) {
         Bundle extras = getIntent().getExtras();
