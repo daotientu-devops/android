486f90531c9a3f21e86d59c268fbcaa63ead3359
tudao <tudao@5nhatnhat.com>
2021-08-23 00:31:13 +0700

    - [Note Sample Project]: Android với SQLite Database (Đã fix và test lại)

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -1,178 +1,121 @@
 package com.example.helloworld;
 
-import android.content.BroadcastReceiver;
-import android.content.Context;
+import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.IntentFilter;
-import android.content.SharedPreferences;
-import android.graphics.Color;
-import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Display;
-import android.view.Gravity;
-import android.view.KeyEvent;
-import android.view.LayoutInflater;
-import android.view.Menu;
+import android.view.ContextMenu;
 import android.view.MenuItem;
-import android.view.OrientationEventListener;
-import android.view.Surface;
 import android.view.View;
-import android.view.ViewGroup;
-import android.view.WindowManager;
-import android.widget.BaseAdapter;
-import android.widget.ImageView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.coordinatorlayout.widget.CoordinatorLayout;
-import androidx.fragment.app.FragmentManager;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-import com.google.android.material.bottomsheet.BottomSheetBehavior;
-import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+import com.example.helloworld.bean.Note;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    TextView txtTest;
-    CoordinatorLayout.LayoutParams txtTestLayout;
-    int themeIdCurrent;
-
-    Display display;
-    TextView huongmanhinh, gocnghieng;
-    OrientationEventListener myOrientationEventListener;
+    private ListView listView;
+    private static final int MENU_ITEM_VIEW = 111;
+    private static final int MENU_ITEM_EDIT = 222;
+    private static final int MENU_ITEM_CREATE = 333;
+    private static final int MENU_ITEM_DELETE = 444;
+    private static final int MY_REQUEST_CODE = 1000;
+    private final List<Note> noteList = new ArrayList<Note>();
+    private ArrayAdapter<Note> listViewAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        huongmanhinh = findViewById(R.id.huongmanhinh);
-        gocnghieng = findViewById(R.id.gocnghieng);
-
-        InfoScreen();
-        ListenerRotate();
-
-        BottomSheetBehavior bottomSheetBehavior = BottomSheetBehavior.from(findViewById(R.id.bottom_sheet));
-        TextView txtTest = findViewById(R.id.txtTest);
-        txtTest.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //Snackbar snackbar = Snackbar.make(view, ((TextView)view).getText(), Snackbar.LENGTH_SHORT);
-                //snackbar.show();
-                FirstBottomSheetDialogFragment myDialog = new FirstBottomSheetDialogFragment();
-                FragmentManager fm = getSupportFragmentManager();
-                myDialog.show(fm, "FirstBottomSheetDialogFragment");
-            }
-        });
-        bottomSheetBehavior.setBottomSheetCallback(bottomSheetCallback);
-        /**
-         * Tạo ra 1 Broadcast Intent sau đó gửi đi (lan truyền) trong ứng dụng
-         * Action ==> Intent ==> Send broadcast
-         */
-        String actionName = "my-first-broadcastintent";
-        Intent intent = new Intent(actionName);
-        // Thiết lập tên để cho Receiver nhận được thì biết đó là loại Intent
-        intent.setAction(actionName);
-        // Dữ liệu gắn vào Intent thiết lập bằng putExtra với định dạng (tên, dữ liệu)
-        // Dữ liệu là các kiểu cơ bản Int, String, ... hoặc các loại đối tượng lớp kế thừa từ serializable
-        intent.putExtra("dataname", "Hello, How are you?");
-        // Thực hiện lan truyền Intent trong hệ thống
-        sendBroadcast(intent);
-
-        // Đọc ID theme đã lưu, nếu chưa lưu thì dùng R.style.MyAppTheme
-        SharedPreferences locationPref = getApplicationContext().getSharedPreferences("MainActivity", MODE_PRIVATE);
-        themeIdCurrent = locationPref.getInt("themeid", R.style.MyAppTheme);
-        // Thiết lập theme cho Activity
-        setTheme(themeIdCurrent);
-        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // chuyển đổi theme
-                themeIdCurrent = themeIdCurrent == R.style.MyAppTheme ? R.style.AppTheme : R.style.MyAppTheme;
-                // Lưu lại theme ID
-                SharedPreferences locationpref = getApplicationContext().getSharedPreferences("MainActivity", MODE_PRIVATE);
-                SharedPreferences.Editor spedit = locationpref.edit();
-                spedit.putInt("themeid", themeIdCurrent);
-                spedit.apply();
-                // Tạo lại activity để áp dụng theme mới đổi
-                recreate();
-            }
-        });
+        // Get listview object from xml
+        this.listView = (ListView) findViewById(R.id.listView);
+        MyDatabaseHelper db = new MyDatabaseHelper(this);
+        db.createDefaultNotesIfNeed();
+        List<Note> list = db.getAllNotes();
+        this.noteList.addAll(list);
+        // Define a new Adapter
+        // 1 - Context
+        // 2 - Layout for the row
+        // 3 - ID of the TextView to which the data is written
+        // 4 - the List of data
+        this.listViewAdapter = new ArrayAdapter<Note>(this, android.R.layout.simple_list_item_1, android.R.id.text1, this.noteList);
+        // Assign adapter to ListView
+        this.listView.setAdapter(this.listViewAdapter);
+        // Register the ListView for Context Menu
+        registerForContextMenu(this.listView);
     }
-    // Listener nhận sự kiện thay đổi góc nghiêng điện thoại
-    void ListenerRotate() {
-        myOrientationEventListener = new OrientationEventListener(this, SensorManager.SENSOR_DELAY_NORMAL) {
-            @Override
-            public void onOrientationChanged(int orientation) {
-                gocnghieng.setText("Góc nghiêng: " + orientation + "o");
-                InfoScreen();
-            }
-        };
-        myOrientationEventListener.enable();
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View view, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, view, menuInfo);
+        menu.setHeaderTitle("Select The Action");
+        // groupId, itemId, order, title
+        menu.add(0, MENU_ITEM_VIEW , 0, "View Note");
+        menu.add(0, MENU_ITEM_CREATE , 1, "Create Note");
+        menu.add(0, MENU_ITEM_EDIT , 2, "Edit Note");
+        menu.add(0, MENU_ITEM_DELETE, 4, "Delete Note");
     }
-    // Xác định màn hình PORTRAIT, LANDSCAPE
-    void InfoScreen() {
-        Display display = ((WindowManager)getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
-        switch (display.getRotation()) {
-            case Surface.ROTATION_0:
-                huongmanhinh.setText("Màn hình LAYOUT đứng: 0o");
-                break;
-            case Surface.ROTATION_90:
-                huongmanhinh.setText("Màn hình LAYOUT ngang: 90o");
-                break;
-            case Surface.ROTATION_180:
-                huongmanhinh.setText("Màn hình LAYOUT ngang: 180o");
-                break;
-            case Surface.ROTATION_270:
-                huongmanhinh.setText("Màn hình LAYOUT đứng: 270o");
-                break;
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        final Note selectedNote = (Note) this.listView.getItemAtPosition(info.position);
+        if (item.getItemId() == MENU_ITEM_VIEW) {
+            Toast.makeText(getApplicationContext(), selectedNote.getNoteContent(), Toast.LENGTH_LONG).show();
+        } else if (item.getItemId() == MENU_ITEM_CREATE) {
+            Intent intent = new Intent(this, AddEditNoteActivity.class);
+            // Start AddEditNoteActivity, (with feedback)
+            this.startActivityForResult(intent, MY_REQUEST_CODE);
+        } else if (item.getItemId() == MENU_ITEM_EDIT) {
+            Intent intent = new Intent(this, AddEditNoteActivity.class);
+            intent.putExtra("note", selectedNote);
+            // Start AddEditNoteActivity, (with feedback)
+            this.startActivityForResult(intent, MY_REQUEST_CODE);
+        } else if (item.getItemId() == MENU_ITEM_DELETE) {
+            // Ask before deleting
+            new AlertDialog.Builder(this)
+                    .setMessage(selectedNote.getNoteTitle() + ". Are you sure you want to delete?")
+                    .setCancelable(false)
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            deleteNote(selectedNote);
+                        }
+                    })
+                    .setNegativeButton("No", null).show();
+        } else {
+            return false;
         }
+        return true;
     }
-    BottomSheetBehavior.BottomSheetCallback bottomSheetCallback = new BottomSheetBehavior.BottomSheetCallback() {
-        @Override
-        public void onStateChanged(@NonNull View bottomSheet, int newState) {
-            switch (newState) {
-                case BottomSheetBehavior.STATE_HIDDEN:
-                    // View bị ẩn
-                    break;
-                case BottomSheetBehavior.STATE_EXPANDED:
-                    // View mở rộng lên
-                    Toast.makeText(bottomSheet.getContext(), "Mở rộng", Toast.LENGTH_SHORT).show();
-                    break;
-                case BottomSheetBehavior.STATE_DRAGGING:
-                    // Bắt đầu kéo View
-                    break;
-                case BottomSheetBehavior.STATE_COLLAPSED:
-                    // View thu gọn lại
-                    Toast.makeText(bottomSheet.getContext(), "Thu gọn", Toast.LENGTH_SHORT).show();
-                    break;
-                default:
-                    break;
-            }
-        }
 
-        @Override
-        public void onSlide(@NonNull View bottomSheet, float slideOffset) {
-            // Vị trí tương đối của View khi trượt, slideOffset = -1 đến 1
-            // Khi nhận từ 0 -> 1 thì sheet đang chuyển từ thu hẹp sang mở rộng
-            // code sau sẽ làm mờ mainlayout khi kéo sheet lên và ngược lại
-            float alpha = 1 - slideOffset;
-            View mainlayout = findViewById(R.id.mainlayout);
-            if (alpha >= 0.5)
-                mainlayout.animate().alpha(alpha).start();
-        }
-    };
-    public static class FirstBottomSheetDialogFragment extends BottomSheetDialogFragment {
-        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-            // Tạo View của Fragment
-            return inflater.inflate(R.layout.fragment_bottom_sheet, container);
+    // Delete a record
+    private void deleteNote(Note note) {
+        MyDatabaseHelper db = new MyDatabaseHelper(this);
+        db.deleteNote(note);
+        this.noteList.remove(note);
+        // Refresh ListView
+        this.listViewAdapter.notifyDataSetChanged();
+    }
+    @Override
+    // When AddEditNoteActivity completed, it sends feedback
+    // (If you start it using startActivityForResult())
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK && requestCode == MY_REQUEST_CODE) {
+            boolean needRefresh = data.getBooleanExtra("needRefresh", true);
+            // Refresh ListView
+            if (needRefresh) {
+                this.noteList.clear();
+                MyDatabaseHelper db = new MyDatabaseHelper(this);
+                List<Note> list = db.getAllNotes();
+                this.noteList.addAll(list);
+                // notify the data change (to refresh the listview)
+                this.listViewAdapter.notifyDataSetChanged();
+            }
         }
     }
 }
